81f310b8aae1c631d6540f610f5100c99b32c22f
Tuğba <tugba.olcer@portalgrup.com>
2024-07-27 22:37:47 +0300

    updated: jdk version is 17

--- curvedBottomNavLibrary/build.gradle.kts
+++ curvedBottomNavLibrary/build.gradle.kts
@@ -22,11 +22,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     viewBinding {
         enable = true
@@ -52,7 +52,7 @@ publishing {
 
             groupId = "com.github.tugbaolcer"
             artifactId = "CurvedBottomNavLib"
-            version = "1.0.3"
+            version = "1.0.4"
 
             pom {
                 name.set("CurvedBottomNavLib")
